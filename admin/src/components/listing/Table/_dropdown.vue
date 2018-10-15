d56425f3478040304325654e0b5cd3047b2e8316
Jack <jack@jacks.site>
2018-10-15 19:06:31 +0200

    converted dropdown into a component

---
+++ admin/src/components/listing/Table/_dropdown.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="btn-group">
+    <a :href="`/content/edit/${record._id}`" class="btn btn-light" ><i class="fa fa-pencil"></i>  Edit</a>
+    <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <span class="sr-only">Toggle Dropdown</span>
+    </button>
+    <div class="dropdown-menu dropdown-menu-right">
+      <button class="dropdown-item" href="#" type="button">
+        <i class="fa fa-trash-o"></i> delete record
+      </button>
+      <button class="dropdown-item" href="#" type="button">
+        <i class="fa fa-clone"></i> duplicate record
+      </button>
+      <div class="dropdown-divider"></div>
+      <h6 class="dropdown-header">status:</h6>
+      <span v-for="(status, index) in record.statuses" :key="index">
+        <template v-if="status !== record.status">
+          <button class="dropdown-item" type="button">
+            {{status}}
+          </button>
+        </template>
+      </span>
+      <div class="dropdown-divider"></div>
+      <h6 class="dropdown-header">meta:</h6>
+      <p class="dropdown-item"><i class="fa fa-star"></i> {{record.createdAt|datetime}}</p>
+      <p class="dropdown-item"><i class="fa fa-calendar"></i> {{record.publishedAt|datetime}}</p>
+      <p class="dropdown-item"><i class="fa fa-pencil"></i>  {{record.modifiedAt|datetime}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "listing-record-dropdown",
+  props: ["record"]
+};
+</script>
--- admin/src/components/listing/Table/_record.vue
+++ admin/src/components/listing/Table/_record.vue
@@ -18,44 +18,20 @@
     <td>image</td>
     <!-- Actions -->
     <td>
-      <div class="btn-group">
-        <a :href="`/content/edit/${record._id}`" class="btn btn-light" ><i class="fa fa-pencil"></i>  Edit</a>
-        <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <span class="sr-only">Toggle Dropdown</span>
-        </button>
-        <div class="dropdown-menu dropdown-menu-right">
-          <button class="dropdown-item" href="#" type="button">
-            <i class="fa fa-trash-o"></i> delete record
-          </button>
-          <button class="dropdown-item" href="#" type="button">
-            <i class="fa fa-clone"></i> duplicate record
-          </button>
-          <div class="dropdown-divider"></div>
-          <h6 class="dropdown-header">status:</h6>
-          <span v-for="(status, index) in record.statuses" :key="index">
-            <template v-if="status !== record.status">
-              <button class="dropdown-item" type="button">
-                {{status}}
-              </button>
-            </template>
-          </span>
-          <div class="dropdown-divider"></div>
-          <h6 class="dropdown-header">meta:</h6>
-          <p class="dropdown-item"><i class="fa fa-star"></i> {{record.createdAt|datetime}}</p>
-          <p class="dropdown-item"><i class="fa fa-calendar"></i> {{record.publishedAt|datetime}}</p>
-          <p class="dropdown-item"><i class="fa fa-pencil"></i>  {{record.modifiedAt|datetime}}</p>
-        </div>
-      </div>
+      <listing-record-dropdown 
+        :record="record"
+      ></listing-record-dropdown>
     </td>
   </tr>
 </template>
 
 <script>
+  import Dropdown from './_dropdown.vue';
   export default {
     name: 'listing-record',
     props: ["record"],
-    mounted() {
-      console.log(this.record);
-    }
+    components: {
+      "listing-record-dropdown": Dropdown
+    },
   }
 </script>
