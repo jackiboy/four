ed5287580f09990ff60d1d07642a023c2a25e7fc
Jack <jack@jacks.site>
2018-10-15 19:08:25 +0200

    but record into folder

---
+++ assets/js/Components/editor/Slug.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="location__plans">
+       slug
+    </div>
+</template>
+
+<script>
+export default {
+  name: "editor-field-slug",
+  mounted(){
+    console.log("mounted")
+  }
+};
+</script>
