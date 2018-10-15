aab3023920331e5c9f91c682d49a3771a5afd21e
Jack <jack@jacks.site>
2018-10-15 19:56:19 +0200

    fixed row

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -6,5 +6,5 @@
 </template>
 
 <style>
-@import "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+  @import "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
 </style>
